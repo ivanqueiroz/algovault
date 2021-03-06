b4e36f7f6f9147a5e5f92aad302b560d280c4f36
Ivan Queiroz <ifbomfim@indracompany.com>
2021-03-06 15:24:38 -0300

    Sort with intercalate.

--- src/main/java/dev/ivanqueiroz/algovault/sort/Sort.java
+++ src/main/java/dev/ivanqueiroz/algovault/sort/Sort.java
@@ -51,4 +51,81 @@ public class Sort {
         products[i] = targetProduct;
     }
 
+    public static Product[] merge(Product[] products1, Product[] products2) {
+        int length = products1.length + products2.length;
+        Product[] result = new Product[length];
+        int current = 0;
+        int indexProduct1 = 0;
+        int indexProduct2 = 0;
+        while (indexProduct1 < products1.length && indexProduct2 < products2.length) {
+            log.info("Checking left element.");
+            Product product1 = products1[indexProduct1];
+            Product product2 = products2[indexProduct2];
+            if (product1.getPrice() < product2.getPrice()) {
+                result[current] = product1;
+                indexProduct1++;
+            } else {
+                result[current] = product2;
+                indexProduct2++;
+            }
+            current++;
+        }
+        while (indexProduct1 < products1.length) {
+            result[current] = products1[indexProduct1];
+            indexProduct1++;
+            current++;
+        }
+        while (indexProduct2 < products2.length) {
+            result[current] = products2[indexProduct2];
+            indexProduct2++;
+            current++;
+        }
+        log.info("Merged array: {}", Arrays.toString(result));
+        return result;
+    }
+
+    public static void intercalate(Product[] products, int start, int midpoint, int end) {
+        final int auxLenght = end - start;
+        Product[] auxArray = new Product[auxLenght];
+        int mainPosition = 0;
+        int indexProduct1 = start;
+        int indexProduct2 = midpoint;
+
+        while (indexProduct1 < midpoint && indexProduct2 < end) {
+            Product product1 = products[indexProduct1];
+            Product product2 = products[indexProduct2];
+            if (product1.getPrice() < product2.getPrice()) {
+                auxArray[mainPosition] = product1;
+                indexProduct1++;
+            } else {
+                auxArray[mainPosition] = product2;
+                indexProduct2++;
+            }
+            mainPosition++;
+        }
+        while (indexProduct1 < midpoint) {
+            auxArray[mainPosition] = products[indexProduct1];
+            indexProduct1++;
+            mainPosition++;
+        }
+        while (indexProduct2 < end) {
+            auxArray[mainPosition] = products[indexProduct2];
+            indexProduct2++;
+            mainPosition++;
+        }
+
+        if (mainPosition >= 0)
+            System.arraycopy(auxArray, 0, products, start, mainPosition);
+    }
+
+    public static void sorting(Product[] products, int start, int end) {
+        int lenght = end - start;
+        if (lenght > 1) {
+            int middle = (start + end) / 2;
+            sorting(products, start, middle);
+            sorting(products, middle, end);
+            intercalate(products, start, middle, end);
+        }
+    }
+
 }
--- src/test/java/dev/ivanqueiroz/algovault/sort/SortTest.java
+++ src/test/java/dev/ivanqueiroz/algovault/sort/SortTest.java
@@ -1,18 +1,29 @@
 package dev.ivanqueiroz.algovault.sort;
 
 import dev.ivanqueiroz.algovault.model.Product;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Slf4j
 class SortTest {
 
     private Product[] products;
+    private Product[] products2;
+    private Product[] products3;
 
     @BeforeEach void setUp() {
         products = new Product[] {new Product("Lamborghini", 1000000), new Product("Jipe", 46000), new Product("Brasília", 16000), new Product("Smart", 46000),
             new Product("Fusca", 17000)};
+        products2 = new Product[] {new Product("Porche", 2000000), new Product("Hammer", 56000), new Product("Celta", 26000), new Product("Ecosport", 86000),
+            new Product("Cherry QQ", 12000)};
+        products3 = Stream.concat(Arrays.stream(products), Arrays.stream(products2)).toArray(Product[]::new);
     }
 
     @Test void selectionSortTest() {
@@ -28,5 +39,23 @@ class SortTest {
     private void sortAssert() {
         assertEquals("Brasília", products[0].getName());
         assertEquals("Lamborghini", products[products.length - 1].getName());
+    }
+
+    @Test void merge() {
+        Comparator<Product> priceComaparator = Comparator.comparingDouble(Product::getPrice);
+        Arrays.sort(products, priceComaparator);
+        Arrays.sort(products2, priceComaparator);
+        final Product[] mergeProducts = Sort.merge(products, products2);
+        final int total = products.length + products2.length;
+        assertEquals(total, mergeProducts.length);
+        assertEquals("Cherry QQ", mergeProducts[0].getName());
+        assertEquals("Porche", mergeProducts[mergeProducts.length - 1].getName());
+    }
+
+    @Test void sorting() {
+
+        Sort.sorting(products3, 0, products3.length);
+        log.info(Arrays.toString(products3));
+        assertEquals("Cherry QQ", products3[0].getName());
     }
 }
